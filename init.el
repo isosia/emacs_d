13e3f7f5edfc3a7315d0373862052cc13af1b847
Itoshi Naramura <isosia.jwrw@gmail.com>
2016-09-28 01:16:54 +0900

    Modify el-get, package

--- init.el
+++ init.el
@@ -3,10 +3,11 @@
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
 ;;; ロードパス追加設定
-(add-to-list 'load-path (locate-user-emacs-file "elisp/el-get/el-get/"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get/"))
 
-(setq el-get-dir "~/.emacs.d/elisp/el-get/")
+(setq el-get-dir "~/.emacs.d/el-get/")
 
+(setq el-get-generate-autoloads t)
 ;;; el-getが無かった場合は，クローンしてくる
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -19,8 +20,30 @@
 (setq el-get-user-package-directory (locate-user-emacs-file "init"))
 
 ;;;
-(require 'el-get)
-(provide 'el-get-init)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
+(defvar my/el-get-packages
+  '(howm))
+(el-get 'sync my/el-get-packages)
 
+
+
+(require 'cl-lib)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my/packages
+  '(
+    yaml-mode yasnippet
+    ))
+(let ((not-installed
+       (cl-loop for x in my/packages
+                when (not (package-installed-p x))
+                collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
